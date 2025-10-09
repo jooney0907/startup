ce4778924673636839484699ce51c54fad01e9de
Sangjoon Ryu <sangjoonryu@gmail.com>
2025-10-08 19:21:16 -0600

    app updates

--- src/app.jsx
+++ src/app.jsx
@@ -4,20 +4,48 @@ import './app.css';
 
 export default function App() {
   return (
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <link rel="icon" href="/favicon.ico" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="theme-color" content="#000000" />
+    <div className="body bg-dark text-light">
+      <header>
+        <h1>
+          Quiztopia<sup>®</sup>
+        </h1>
+        <nav className="nav">
+          <a className="nav-link" href="scores.html">High Scores</a>
+          <a className="nav-link" href="index.html">Home</a>
+          <a className="nav-link" href="about.html">About</a>
+        </nav>
+        <hr />
+      </header>
 
-    <title>Simon React</title>
-  </head>
-  <body>
-    <noscript>You need to enable JavaScript to run this app.</noscript>
-    <div id="root"></div>
-    <script type="module" src="/index.jsx"></script>
-  </body>
-</html>
+      <main className="container text-center py-5" style={{ maxWidth: 600 }}>
+        <h3>Play now!</h3>
+
+        <form method="get" action="lobby.html" className="text-start">
+          <div className="input-group mb-3">
+            <input className="form-control" type="text" name="username" placeholder="Username" required />
+          </div>
+
+          <div className="input-group mb-4">
+            <input className="form-control" type="password" name="password" placeholder="Password" required />
+          </div>
+
+          <div className="d-grid gap-2 mb-1">
+            <button type="submit" className="btn btn-primary">Login</button>
+          </div>
+        </form>
+
+        <h3 className="m-0 mt-2 mb-0">Don't have an account? Register now!</h3>
+        <div className="d-grid gap-2 mt-1">
+          <a href="register.html" className="btn btn-primary">Register</a>
+        </div>
+      </main>
+
+      <footer className="container py-4">
+        <hr className="border-secondary" />
+        <div className="d-flex justify-content-end">
+          <a className="text-reset" href="https://github.com/jooney0907/startup">GitHub</a>
+        </div>
+      </footer>
+    </div>
   );
 }
