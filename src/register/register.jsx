5e127b94c2b4e08584d1d1ad81b872d7166dedd8
Sangjoon Ryu <sangjoonryu@gmail.com>
2025-10-08 18:53:14 -0600

    register updates

--- src/register/register.jsx
+++ src/register/register.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import './about.css'
-export function About() {
+import './register.css'
+export function Register() {
   return (
-  <main className="container text-center my-4">
-    <div id="picture" className="picture-box mb-4">
-      <img src="quiz.jpg" alt="Trivia game" className="img-fluid rounded shadow" style="max-width:400px;" />
-    </div>
+<main className="container text-center py-5 login-main">
+    <h3>Sign up!</h3>
 
-    <p className="lead">
-      Quiztopia is a multiplayer trivia game where players can compete in real time.
-      Trivia as a game format has been popular since the 20th century, and Quiztopia
-      brings that tradition into a fun, competitive online space. Challenge friends and
-      family and see who can answer the most questions!
-    </p>
+    <form method="get" action="lobby.html" className="text-start">
+      <div className="input-group mb-3">
+        <input className="form-control" type="text" name="username" placeholder="Username" required />
+      </div>
+
+      <div className="input-group mb-4">
+        <input class="form-control" type="password" name="password" placeholder="Password" required />
+      </div>
+
+      <div className="d-grid gap-2 mb-1">
+        <button type="submit" className="btn btn-primary">Login</button>
+      </div>
+    </form>
+
+    <h3 className="m-0 mt-2 mb-0">Already have an account?</h3>
+    <div className="d-grid gap-2 mt-1">
+      <a href="index.html" className="btn btn-primary">Log in</a>
+    </div>
   </main>
   );
 }
